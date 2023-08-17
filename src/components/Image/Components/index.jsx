e8237fcfaf4ee8210cc9ba5b71bd1b8d4e7f8339
Eylon Shmilovich <eylonshm@gmail.com>
2023-08-17 15:13:35 +0300

    Move Image Component into another folder

---
+++ src/components/Image/Components/index.jsx
@@ -0,0 +1,68 @@
+import { motion } from 'framer-motion'
+import styles from '../index.module.scss'
+import classNames from 'classnames'
+import {
+  imgVariants,
+  titleVariants,
+  titleWrapperVariants,
+  footerBackgroundVariants,
+  roleVariants,
+} from './animationVariants'
+
+function Photo() {
+  return (
+    <motion.img
+      src={
+        'https://media.istockphoto.com/id/656673020/photo/handsome-in-spectacles.jpg?s=612x612&w=0&k=20&c=klIlcueGJlAnOehrIZNpvFrVT_olKKqhaVT77H5Fj-s='
+      }
+      variants={imgVariants}
+      transition={{
+        ease: 'linear',
+        duration: 0.18,
+      }}
+    />
+  )
+}
+
+function BeforeHoverTitle() {
+  return (
+    <motion.h3
+      className={classNames(styles.title, styles.outerTitle)}
+      variants={titleVariants}
+    >
+      Maximilian Shwartz
+    </motion.h3>
+  )
+}
+
+function AfterHoverTitle() {
+  return (
+    <motion.div className={styles.titleWrapper} variants={titleWrapperVariants}>
+      <h3 className={styles.title} variants={titleVariants}>
+        Maximilian Shwartz
+      </h3>
+    </motion.div>
+  )
+}
+
+function FooterBackground() {
+  return (
+    <motion.div
+      className={styles.footerBackground}
+      variants={footerBackgroundVariants}
+    />
+  )
+}
+
+function Role() {
+  return (
+    <motion.h3
+      className={classNames(styles.title, styles.role)}
+      variants={roleVariants}
+    >
+      Head of Design
+    </motion.h3>
+  )
+}
+
+export { Photo, BeforeHoverTitle, AfterHoverTitle, FooterBackground, Role }
