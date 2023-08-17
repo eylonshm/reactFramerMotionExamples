b2a85c664da11c2ab85328f7976c693bfd851e81
Eylon Shmilovich <eylonshm@gmail.com>
2023-08-17 13:30:59 +0300

    Add Scroll to top button animation

--- src/components/Button/index.jsx
+++ src/components/Button/index.jsx
@@ -1,14 +1,18 @@
 import { motion } from 'framer-motion'
 import styles from './index.module.scss'
+import classNames from 'classnames'
 
-const Button = () => {
+const Button = (props) => {
+  const { onClick = () => {}, text, className } = props
   return (
     <motion.button
-      className={styles.button}
+      {...props}
+      onClick={onClick}
+      className={classNames(styles.button, className)}
       whileHover={{ scale: 1.1 }}
       whileTap={{ scale: 0.8 }}
     >
-      Click Me
+      {text || 'Click Me'}
     </motion.button>
   )
 }
--- src/components/Scroll/index.jsx
+++ src/components/Scroll/index.jsx
@@ -1,8 +1,23 @@
-import { motion, useScroll, useSpring } from 'framer-motion'
+import React, { useRef } from 'react'
+import { motion, useScroll, useSpring, useInView } from 'framer-motion'
 import styles from './index.module.scss'
 import LoremIpsum from '../LoremIpsum'
+import { Button } from '../index'
+
+const headerVariants = {
+  headerNotInView: {
+    opacity: 0.5,
+  },
+
+  headerInView: {
+    opacity: 1,
+    scale: 1.5,
+  },
+}
 
 const Scroll = () => {
+  const headerRef = useRef(null)
+  const isHeaderInView = useInView(headerRef)
   const { scrollYProgress } = useScroll()
   const scaleX = useSpring(scrollYProgress, {
     stiffness: 100,
@@ -10,11 +25,41 @@ const Scroll = () => {
     restDelta: 0.001,
   })
 
+  const scrollToTop = () =>
+    headerRef.current.scrollIntoView({
+      behavior: 'smooth',
+    })
+
   return (
     <div className={styles.wrapper} style={{ scaleX }}>
       <motion.div className={styles.progressBar} style={{ scaleX }} />
-      <h1>Scroll Spring animation</h1>
+
+      <motion.h1
+        initial='headerNotInView'
+        whileInView='headerInView'
+        ref={headerRef}
+        variants={headerVariants}
+        className={styles.header}
+      >
+        Scroll Spring animation
+      </motion.h1>
+
       <LoremIpsum />
+
+      {!isHeaderInView && (
+        <Button
+          className={styles.jumpButton}
+          text='UP'
+          onClick={scrollToTop}
+          initial={{ opacity: 0, y: 50 }}
+          animate={{ opacity: 1, y: 0 }}
+          transition={{
+            type: 'spring',
+            stiffness: 100,
+            damping: 10,
+          }}
+        />
+      )}
     </div>
   )
 }
