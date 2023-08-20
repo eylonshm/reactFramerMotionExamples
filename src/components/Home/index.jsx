8215e4bde730ec72b1507d90c54ba633b2f642ed
Eylon Shmilovich <eylonshm@gmail.com>
2023-08-20 14:16:20 +0300

    Add HomeScreen DarkTheme

--- src/components/Home/index.jsx
+++ src/components/Home/index.jsx
@@ -1,8 +1,9 @@
 import React from 'react'
+import styles from './index.module.scss'
 
 const Home = () => {
   return (
-    <div>
+    <div className={styles.wrapper}>
       <h1>Just Move between animations using the navBar plz.</h1>
     </div>
   )
